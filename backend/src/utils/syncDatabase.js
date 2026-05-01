eadbe028bd9e9aebdf04086f1d8a9181863c7e4e
Shivam9572 <shivamroy9572@gamil.com>
2026-05-01 18:49:55 +0530

    sync:false

--- backend/src/utils/syncDatabase.js
+++ backend/src/utils/syncDatabase.js
@@ -2,7 +2,7 @@ const { sequelize } = require("../models");
 
 async function syncDatabase() {
   try {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ alter: false });
     console.log("Database synced successfully.");
   } catch (error) {
     console.error("Database sync failed:", error);
