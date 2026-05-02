95e12ae4a39cde421dcc204d1da6279a8e7c6324
Shivam9572 <shivamroy9572@gamil.com>
2026-05-03 00:26:17 +0530

    fix: hardcode backend URL temporarily

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // ✅ fallback hatao
+  baseURL:"https://projectmanagemnet-production.up.railway.app/api", // ✅ fallback hatao
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
